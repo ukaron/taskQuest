d916314ca9c02765528e72c95a5bd5047dc8e8e8
nikitakhokhlov <nikita@mimic.digital>
2024-12-13 11:33:25 +0700

    header

--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -1,5 +1,6 @@
 import { Breadcrumbs } from "@/features/breadcrumbs";
 import { auth } from "@/shared/lib/firebaseConfig";
+import { Avatar, AvatarFallback, AvatarImage } from "@/shared/ui/avatar";
 import { Button } from "@/shared/ui/button";
 import {
   DropdownMenu,
@@ -9,12 +10,13 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/shared/ui/dropdown-menu";
-import { useNavigate } from "@tanstack/react-router";
+import { useNavigate, useLocation } from "@tanstack/react-router";
 import { signOut } from "firebase/auth";
 import { ChevronLeft } from "lucide-react";
 
 export const Header = () => {
   const navigator = useNavigate();
+  const location = useLocation();
 
   const onLogout = () => {
     localStorage.removeItem("token");
@@ -23,37 +25,47 @@ export const Header = () => {
   };
 
   return (
-    <header className="sticky w-full top-0 z-30 flex sm:py-4 h-14 justify-between items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6 lg:px-8">
+    <header
+      className={`sticky w-full top-0 z-30 flex sm:py-4 h-14
+        items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6 lg:px-8
+        ${location.pathname !== "/" ? "justify-between" : "justify-end"}
+        `}
+    >
       <Breadcrumbs />
-      <Button
-        variant="outline"
-        size="icon"
-        className="h-7 w-7 md:hidden flex"
-        onClick={() => {
-          navigator({ to: "../" });
-        }}
-      >
-        <ChevronLeft className="h-4 w-4" />
-        <span className="sr-only">Back</span>
-      </Button>
+      {location.pathname !== "/" && (
+        <Button
+          variant="outline"
+          size="icon"
+          className="h-7 w-7 md:hidden flex"
+          onClick={() => {
+            navigator({ to: "../" });
+          }}
+        >
+          <ChevronLeft className="h-4 w-4" />
+          <span className="sr-only">Back</span>
+        </Button>
+      )}
 
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
-          <Button
-            variant="outline"
-            size="icon"
-            className="overflow-hidden rounded-full"
-          >
-            N H
-          </Button>
+          <Avatar>
+            {auth.currentUser?.photoURL && (
+              <AvatarImage alt="avatar" src={auth.currentUser.photoURL} />
+            )}
+            <AvatarFallback>
+              {auth.currentUser?.displayName
+                ?.split(" ")
+                .map((chunk) => chunk[0])
+                .join("")}
+            </AvatarFallback>
+          </Avatar>
         </DropdownMenuTrigger>
         <DropdownMenuContent align="end">
           <DropdownMenuLabel>My Account</DropdownMenuLabel>
           <DropdownMenuSeparator />
           <DropdownMenuItem>Settings</DropdownMenuItem>
-          <DropdownMenuItem>Support</DropdownMenuItem>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem onClick={onLogout}>Logout</DropdownMenuItem>
+          {/* <DropdownMenuItem>Support</DropdownMenuItem> */}
+          {/* <DropdownMenuItem onClick={onLogout}>Logout</DropdownMenuItem> */}
         </DropdownMenuContent>
       </DropdownMenu>
     </header>
